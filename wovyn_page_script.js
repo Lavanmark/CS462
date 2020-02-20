a3a60a3b3a4d1b13c1a24e6687059da3d15d5919
Tyler Draughon <tdraughon@byu.edu>
2020-02-19 22:19:51 -0700

    Update wovyn_page_script.js

--- wovyn_page_script.js
+++ wovyn_page_script.js
@@ -1,49 +1,68 @@
-angular.module('timing', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
-    $scope.timings = [];
-    $scope.eci = "cj1i5z6240003s5ddpomhahb6";
+angular.module('wovyn_page_script', [])
+  .controller('MainCtrl', [
+    '$scope', '$http',
+    function ($scope, $http) {
+      $scope.currentTemperature = -400.0;
+      $scope.name = "no_name";
+      $scope.phone = "12345678901";
+      $scope.location = "location_unknown";
+      $scope.temperatureThreshold = 75;
+      $scope.temperatures = new Map();
+      $scope.thresholdViolations = new Map();
 
-    var bURL = '/sky/event/'+$scope.eci+'/eid/timing/started';
-    $scope.addTiming = function() {
-      var pURL = bURL + "?number=" + $scope.number + "&name=" + $scope.name;
-      return $http.post(pURL).success(function(data){
-        $scope.getAll();
-        $scope.number='';
-        $scope.name='';
-      });
-    };
+      $scope.eci = "Uoc3txAdn1AhTAkPnMqbZz";
 
-    var iURL = '/sky/event/'+$scope.eci+'/eid/timing/finished';
-    $scope.finished = function(number) {
-      var pURL = iURL + "?number=" + number;
-      return $http.post(pURL).success(function(data){
-        $scope.getAll();
-      });
-    };
+      $scope.updateProfile = function () {
+        var bURL = 'http://localhost:8080/sky/event/' + $scope.eci + '/eid/sensor/profile_updated';
+        var pURL = bURL + "?phone_number=" + $scope.phone + "&name=" + $scope.name;
+        pURL = pURL + "&threshold=" + $scope.temperatureThreshold + "&location=" + $scope.location;
+        return $http.post(pURL).success(function (data) {
+          $scope.getAllTemps();
+          $scope.getViolations();
+          $scope.getCurrentTemp();
+          $scope.getProfile();
+        });
+      };
 
-    var gURL = '/sky/cloud/'+$scope.eci+'/timing_tracker/entries';
-    $scope.getAll = function() {
-      return $http.get(gURL).success(function(data){
-        angular.copy(data, $scope.timings);
-      });
-    };
+      $scope.getAllTemps = function () {
+        var gURL = 'http://localhost:8080/sky/cloud/' + $scope.eci + '/temperature_store/temperatures';
+        return $http.get(gURL).success(function (data) {
+          $scope.temperatures = data;
+        });
+      };
 
-    $scope.getAll();
+      $scope.getCurrentTemp = function () {
+        var gURL = 'http://localhost:8080/sky/cloud/' + $scope.eci + '/temperature_store/current_temperature';
+        return $http.get(gURL).success(function (data) {
+          console.log(data);
+          $scope.currentTemperature = Number(data);
+        });
+      };
 
-    $scope.timeDiff = function(timing) {
-      var bgn_sec = Math.floor(Date.parse(timing.time_out)/1000);
-      var end_sec = Math.floor(Date.parse(timing.time_in)/1000);
-      var sec_num = end_sec - bgn_sec;
-      var hours   = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
-  
-      if (hours   < 10) {hours   = "0"+hours;}
-      if (minutes < 10) {minutes = "0"+minutes;}
-      if (seconds < 10) {seconds = "0"+seconds;}
-      return hours+':'+minutes+':'+seconds;
+
+      $scope.getViolations = function () {
+        var vURL = 'http://localhost:8080/sky/cloud/' + $scope.eci + '/temperature_store/threshold_violations';
+        return $http.get(vURL).success(function (data) {
+          $scope.thresholdViolations = data;
+        });
+      };
+
+
+      $scope.getProfile = function () {
+        var profileURL = 'http://localhost:8080/sky/cloud/' + $scope.eci + '/sensor_profile/get_profile';
+        return $http.get(profileURL).success(function (data) {
+          $scope.name = data['name']
+          $scope.phone = data['phone']
+          $scope.location = data['location']
+          $scope.temperatureThreshold = data['threshold']
+        });
+      };
+
+      $scope.runAll = function () {
+        $scope.getProfile();
+        $scope.getAllTemps();
+        $scope.getViolations();
+        $scope.getCurrentTemp();
+      };
     }
-  }
-]);
+  ]);
